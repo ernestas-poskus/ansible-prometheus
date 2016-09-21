75d8b525029d1e5f02c7ae62ea2f677cf1b8578f
Ernestas Poskus <ernestas.poskus@gmail.com>
2016-09-21 23:47:29 +0300

    Format parse_config ruby script

--- parse_config.rb
+++ parse_config.rb
@@ -1,13 +1,18 @@
 input = <<eos
 eos
 
-puts input.gsub("\t", '# ').gsub('  -', '').gsub('    ', '').
-  gsub(' string', ':')
-  gsub(' int', ':')
+puts input.
+  gsub("\t", '# ').
+  gsub('  -', '').
+  gsub('    ', '').
+  gsub(' string', ':').
+  gsub(' int', ':').
   gsub(' value', ':')
 
 require 'yaml'
+puts '############################################################'
 puts '# Auto generated'
+puts '############################################################'
 YAML.load(input).each do |k, v|
   puts "#{k}: {{ #{k.gsub('.', '__').gsub('-', '_')} }}"
 end
